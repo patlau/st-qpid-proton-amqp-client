6b080d6751cbaf0c653214e47bd1f5f219d131f2
Patrick Lauper <plauper@yahoo.com>
2015-10-21 16:15:55 +0200

    Started OS8 implementation

--- OS8-Test.bat
+++ OS8-Test.bat
@@ -2,7 +2,8 @@ SET OSTUDIOEXE=C:\Program Files (x86)\Cincom\ObjectStudio8.7\ObjectStudio.exe
 SET IMAGE=C:\ProgramData\Cincom\ObjectStudio8.7\ObjectStudio.im
 SET LOADFILE=%TEMP%\OS7Load.txt
 
-echo %~dp0\OS\test\ApplicationLoader.cls > %LOADFILE%
+REM echo %~dp0\OS\test\ApplicationLoader.cls > %LOADFILE%
+echo _include %~dp0OS\OS8-Messaging.txt > %LOADFILE%
 
 cd OS
 start "ObjectStudio" "%OSTUDIOEXE%" "%IMAGE%" -ostudio "-l%LOADFILE%"
